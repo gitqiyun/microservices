27fff4c720f3d09f3dda4df1d25ed1ce94fbb0ee
gitqiyun <1114260199@qq.com>
2019-10-23 14:53:41 +0800

    添加pay项目

---
+++ shop-pay-web/src/test/java/com/pay/shoppayweb/ShopPayWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.shoppayweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPayWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ shop-pay/src/main/java/com/qy/shoppay/ShopPayApplication.java
@@ -0,0 +1,13 @@
+package com.qy.shoppay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopPayApplication.class, args);
+    }
+
+}
